1e74fea3b76070a0d51dce6354b42ae488076cc2
Sanika03 <sanikasuryawanshi0305@gmail.com>
2022-09-13 09:00:47 +0530

    corrected answer of a question

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ var quizForm = document.querySelector(".quiz-form");
 var submitAnsBtn = document.querySelector("#submit-ans-btn");
 var outputDiv = document.querySelector("#output-div");
 
-var correctAnswers = ["90°","obtuse", "right-angled", "isosceles", "15cm"];
+var correctAnswers = ["90°","acute", "right-angled", "isosceles", "15cm"];
 
 function checkAnswers(){
     let score = 0;
